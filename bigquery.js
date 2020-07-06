f1e156f4c515268a439566b740805a1a53d4d660
Danil Dubovik <d.dubovyk@nodeart.io>
2020-07-06 18:03:34 +0300

    refactor: insert to stream

--- bigquery.js
+++ bigquery.js
@@ -1,7 +1,22 @@
 const { BigQuery } = require('@google-cloud/bigquery');
+const fs = require('fs');
+const JSONStream = require('JSONStream');
+const es = require('event-stream');
 require('dotenv').config();
 
 const { settlementsSchema } = require('./config/bigquery');
+
+const FIRST_LEVEL = 'Перший рівень';
+const SECOND_LEVEL = 'Другий рівень';
+const THIRD_LEVEL = 'Третій рівень';
+const FOURTH_LEVEL = 'Четвертий рівень';
+const CATEGORY = 'Категорія';
+const NAME = "Назва об'єкта українською мовою";
+
+const levels = {};
+
+const datasetId = process.env.DATASET_NAME;
+const tableId = process.env.TABLE;
 
 const db = new BigQuery({
   projectId: process.env.PROJECT_ID,
@@ -15,29 +30,102 @@ const db = new BigQuery({
 });
 
 module.exports.createDataset = async () => {
-  return await db.createDataset('labreports');
+  return await db.createDataset(datasetId);
 };
 
-module.exports.createTable = async () => {
+const createTable = async () => {
   const options = {
     schema: settlementsSchema,
   };
-  const [table] = await db.dataset('labreports').createTable('koatuu', options);
+  const [table] = await db.dataset(datasetId).createTable(tableId, options);
   return table;
 };
 
 module.exports.getTable = () => {
   return new Promise(async (resolve, reject) => {
     try {
-      const dataset = db.dataset('labreports');
-      const [table] = await dataset.table('koatuu').get();
-      resolve(table);
+      const exists = await db.dataset(datasetId).table(tableId).exists();
+      if (exists[0]) {
+        await db.dataset(datasetId).table(tableId).delete();
+        const table = await createTable();
+        resolve(table);
+      } else {
+        throw Error('table does not exist');
+      }
     } catch (err) {
-      reject('table does not exist');
+      try {
+        const table = await createTable();
+        resolve(table);
+      } catch (err) {
+        reject(err);
+      }
     }
   });
 };
 
-module.exports.insertData = async (rows) => {
-  return await db.dataset('labreports').table('koatuu').insert(rows);
+module.exports.insertData = (file) => {
+  const parser = JSONStream.parse('*');
+  fs.createReadStream(file)
+    .pipe(parser)
+    .pipe(
+      es.mapSync((data) => {
+        if (
+          data[FIRST_LEVEL] &&
+          !data[SECOND_LEVEL] &&
+          !data[THIRD_LEVEL] &&
+          !data[FOURTH_LEVEL] &&
+          !levels[data[FIRST_LEVEL]]
+        ) {
+          levels[data[FIRST_LEVEL]] = data[NAME];
+        }
+
+        if (
+          data[SECOND_LEVEL] &&
+          +data[SECOND_LEVEL].toString().slice(3, 5) &&
+          !data[THIRD_LEVEL] &&
+          !data[FOURTH_LEVEL] &&
+          !levels[data[SECOND_LEVEL]]
+        ) {
+          levels[data[SECOND_LEVEL]] = data[NAME];
+        }
+
+        if (
+          data[THIRD_LEVEL] &&
+          +data[THIRD_LEVEL].toString().slice(6, 8) &&
+          !data[FOURTH_LEVEL] &&
+          !levels[data[THIRD_LEVEL]]
+        ) {
+          levels[data[THIRD_LEVEL]] = data[NAME];
+        }
+
+        if (data[CATEGORY]) {
+          const code = [
+            data[FIRST_LEVEL],
+            data[SECOND_LEVEL],
+            data[THIRD_LEVEL],
+            data[FOURTH_LEVEL],
+          ].filter((level) => level);
+
+          const item = {
+            Code: code[code.length - 1].toString(),
+            Region: levels[data[FIRST_LEVEL]],
+            City: levels[data[SECOND_LEVEL]],
+            District: levels[data[THIRD_LEVEL]] || '',
+            Village: data[FOURTH_LEVEL] ? data[NAME] : '',
+          };
+          return JSON.stringify(item) + '\n';
+        }
+      }),
+    )
+    .pipe(
+      db.dataset(datasetId).table(tableId).createWriteStream({
+        sourceFormat: 'NEWLINE_DELIMITED_JSON',
+      }),
+    )
+    .on('error', (e) => {
+      console.error('Error!', e);
+    })
+    .on('complete', (job) => {
+      console.log('All done!', job);
+    });
 };

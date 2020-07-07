7160a1fa5c6d919a9920ad95ac17513652968d24
Danil Dubovik <d.dubovyk@nodeart.io>
2020-07-07 23:24:56 +0300

    refactor: download link and file to env

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const os = require('os');
 require('dotenv').config();
 
 const { getTable, insertData } = require('./bigquery');
+const { fileName } = require('./config/download');
 
 const main = async () => {
   try {
     await getTable();
-    insertData(`${os.tmpdir()}/koatuu.json`);
+    insertData(`${os.tmpdir()}/${fileName}`);
   } catch (err) {
     console.error(err);
   }
--- download.js
+++ download.js
@@ -2,7 +2,12 @@ const https = require('https');
 const fs = require('fs');
 const os = require('os');
 
-const file = fs.createWriteStream(`${os.tmpdir()}/koatuu.json`);
-https.get('https://data.gov.ua/dataset/d945de87-539c-45b4-932a-7dda57daf8d9/resource/296adb7a-476a-40c8-9de6-211327cb3aa1/download/koatuu.json', (response) => {
+require('dotenv').config();
+
+const { downloadLink, fileName } = require('./config/download');
+
+const file = fs.createWriteStream(`${os.tmpdir()}/${fileName}`);
+
+https.get(downloadLink, (response) => {
   response.pipe(file);
 });
